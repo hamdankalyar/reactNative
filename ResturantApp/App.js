7f3869e53a45139f8dd3f22a83c5dd05b6793830
Hamdan Kalyar <hamdankalyar@Hamdans-MacBook-Air.local>
2022-08-02 13:04:55 +0500

    testing purpose

--- ResturantApp/App.js
+++ ResturantApp/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>here is from mac os </Text>
       <StatusBar style="auto" />
     </View>
   );
